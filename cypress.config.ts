004eebb083c23b8accdb39f7aa8f1afc32265e85
Lysa14 <lysaderbah.12@gmail.com>
2024-01-26 13:15:12 +0100

    c

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: './',
+    baseUrl: '',
     specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
   },
 })
