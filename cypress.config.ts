c49fb3c7ae23df389bf40b82a80a5a4e4f7623c9
Lysa14 <lysaderbah.12@gmail.com>
2024-04-11 09:37:39 +0200

    Update Cypress configuration and remove unused files

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {},
-    baseUrl: '/',
-    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
-  },
-})
--- cypress/integration/sample.cy.js
+++
@@ -1,9 +0,0 @@
-// cypress/integration/sample.spec.js
-
-describe('Sample Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/');
-    cy.contains('Welcome to Your App');
-  });
-});
-
